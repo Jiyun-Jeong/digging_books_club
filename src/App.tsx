35c1a2a5fd0b2bcfa8aad42eaf63e9a1e3bec3b3
Jiyun Jeong <rijamong0830@naver.com>
2023-11-18 13:36:39 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 import axios from 'axios';
 
 function App() {
-  const [search, setSearch] = useState<string>("");
+  const [searchKeyword, setSearch] = useState<string>("");
   const [bookList, setBookList] = useState([]);
 
   return (
@@ -15,7 +15,7 @@ function App() {
             <input 
               type="text"
               name="title"
-              value={search} 
+              value={searchKeyword} 
               className="input_text"
               onChange={(e) => {
                 setSearch(e.target.value);
@@ -23,7 +23,7 @@ function App() {
               placeholder="책 검색하기" 
               aria-label="책 이름 입력" />
               <button type="button" className="search_btn" aria-label="검색" onClick={async () => {
-                const list = await axios.get(`/ttb/api/ItemSearch.aspx?ttbkey=ttbrijamong08302251002&Query=${search}&SearchTarget=Book&output=js&maxResults=10`);
+                const list = await axios.get(`/ttb/api/ItemSearch.aspx?ttbkey=ttbrijamong08302251002&Query=${searchKeyword}&SearchTarget=Book&Version=2&output=js&maxResults=10`);
                 setBookList(JSON.parse(list.data.replace(/[\\;]/g, '')).item)}}>
               </button>
           </div> {/* 식이 필요합니다 */}
