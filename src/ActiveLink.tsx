3884b8921e67f1502026a52fcd271d01d13b6a68
Jiyun Jeong <rijamong0830@naver.com>
2024-01-08 16:05:15 +0900

    안쓰는 컴포넌트 주석처리

--- src/ActiveLink.tsx
+++ src/ActiveLink.tsx
@@ -1,31 +1,31 @@
-import {useRouter} from 'next/router'
+// import {useRouter} from 'next/router'
 
-function ActiveLink({children, href}) {
-    const router = useRouter()
-    const [loading, setLoading] = useState<boolean>(false);
-    const style = {
-        marginRight: 10,
-        color: router.asPath === href ? 'red' : 'black',
-    }
+// function ActiveLink({children, href}) {
+//     const router = useRouter()
+//     const [loading, setLoading] = useState<boolean>(false);
+//     const style = {
+//         marginRight: 10,
+//         color: router.asPath === href ? 'red' : 'black',
+//     }
 
-    const movePageClick = e => {
-        e.preventDefault()
-        setLoading(true);
-        router
-            .push(href)
-            .then(() => {
-                setLoading(false);
-            });
-    }
+//     const movePageClick = e => {
+//         e.preventDefault()
+//         setLoading(true);
+//         router
+//             .push(href)
+//             .then(() => {
+//                 setLoading(false);
+//             });
+//     }
 
-    return (
-        {loading && <PageLoading />}
-        <a href={href} onClick={movePageClick}>
-            {children}
-        </a>
-    )
-}
+//     return (
+//         {loading && <PageLoading />}
+//         <a href={href} onClick={movePageClick}>
+//             {children}
+//         </a>
+//     )
+// }
 
-export default ActiveLink
+// export default ActiveLink
 
 //https://velog.io/@khy226/Next.js-Router-%EC%A0%95%EB%A6%AC
